c664ba9f0c4f656391ad0d4d4036f88abef08dca
Axel <axel.gross.2011@gmail.com>
2026-01-20 17:16:16 +0100

    Add sequence length calculation from CSV, JSON, or CLI input

--- hello.py
+++ hello.py
@@ -1,12 +1,35 @@
 import json
+import csv
+import argparse
 
-with open("/mnt/c/Users/axelg/Downloads/proteins.json") as f:
-    data = json.load(f)
+parser = argparse.ArgumentParser()
+parser.add_argument("--csv",)
+parser.add_argument("--json",)
+parser.add_argument("--seq",)
+args = parser.parse_args()
 
-for protein in data["proteins"]:
-    sequence = protein["meta"]["sequence"]
-    nombre_caracteres = len(sequence)
-    name = protein["name"]
-    print(name,":", nombre_caracteres)
-    
-    
+sequences_list = []
+
+if args.csv:
+    with open(args.csv) as f:
+        csv_reader = csv.reader(f)
+        next(csv_reader)
+        for row in csv_reader:
+            sequences_list.append(row[1])
+elif args.json:
+    with open(args.json) as f:
+        data = json.load(f)
+
+        for protein in data["proteins"]:
+            sequence = protein["meta"]["sequence"]
+            sequences_list.append(sequence)
+elif args.seq:
+    sequence = args.seq
+    sequences_list.append(sequence)
+
+def compute_sequence_length(sequences: list):
+    for sequence in sequences:
+        nombre_caracteres = len(sequence)
+        print(f"{sequence} a {nombre_caracteres} caracteres")
+
+compute_sequence_length(sequences_list)
