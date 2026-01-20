904b4fb6b16767dd37c661b5290012c3395c8ee3
Axel <axel.gross.2011@gmail.com>
2026-01-20 13:46:12 +0100

    Add script to count characters per sequence in JSON file

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("hello world")
+import json
+
+with open("/mnt/c/Users/axelg/Downloads/proteins.json") as f:
+    data = json.load(f)
+
+for protein in data["proteins"]:
+    sequence = protein["meta"]["sequence"]
+    nombre_caracteres = len(sequence)
+    name = protein["name"]
+    print(name,":", nombre_caracteres)
+    
+    
